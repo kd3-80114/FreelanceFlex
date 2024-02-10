dc64c856710d8aaa0550f7ac210fddd6a617fb2d
KD3-80111 <nagdeotesarthak@gmail.com>
2024-02-10 15:09:42 +0530

    delete implementing

--- FreelanceFlex/src/main/java/com/app/controller/AdminController.java
+++ FreelanceFlex/src/main/java/com/app/controller/AdminController.java
@@ -3,7 +3,9 @@ package com.app.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,23 +23,30 @@ public class AdminController {
 	@GetMapping("/viewProfile")
 	public ResponseEntity<?> viewProfile(@RequestParam Long id, @RequestParam String email, @RequestParam String Role) {
 		System.out.println(id);
-
 		return ResponseEntity.status(HttpStatus.OK).body(adminService.findById(id));
 	}
+	
 	@PostMapping("/findBuyerProfile")
 	public ResponseEntity<?> findBuyerProfileByEmail(@RequestParam String email)
 	{
 		System.out.println("Inside findProfile by email of Buyer");
 		System.out.println(email);
-		return ResponseEntity.status(HttpStatus.OK).body(adminService.findBuyerByEmail(email));
-		
+		return ResponseEntity.status(HttpStatus.OK).body(adminService.findBuyerByEmail(email));	
 	}
+	
 	@PostMapping("/findFreelancerProfile")
 	public ResponseEntity<?> findFreelancerProfileByEmail(@RequestParam String email)
 	{
 		System.out.println("Inside findProfile by email of Freelancer");
 		System.out.println(email);
-		return ResponseEntity.status(HttpStatus.OK).body(adminService.findFreelancerByEmail(email));
-		
+		return ResponseEntity.status(HttpStatus.OK).body(adminService.findFreelancerByEmail(email));	
 	}
+	
+	@DeleteMapping("/deleteFreelancer/{freelancerId}")
+	public ResponseEntity<?> deleteFreelancer(@PathVariable Long freelancerId){
+		System.out.println("in delete freelancer");
+		System.out.println(freelancerId);
+		return ResponseEntity.status(HttpStatus.OK).body(adminService.deleteFreelancer(freelancerId));
+	}
+	
 }
--- FreelanceFlex/src/main/java/com/app/service/admin/AdminServiceImpl.java
+++ FreelanceFlex/src/main/java/com/app/service/admin/AdminServiceImpl.java
@@ -21,7 +21,6 @@ import com.app.entities.Freelancer;
 @Transactional
 public class AdminServiceImpl implements AdminService{
 	
-	
 	@Autowired
 	private ModelMapper mapper;
 	@Autowired
@@ -30,19 +29,18 @@ public class AdminServiceImpl implements AdminService{
 	private BuyerDao buyerdao;
 	@Autowired
 	private FreelancerDao freelancerdao;
+	
 	@Override
-	public AdminDTO findById(Long id) {
-		
-		return mapper.map(admindao.findById(id)
+	public AdminDTO findById(Long id) {	
+	return mapper.map(admindao.findById(id)
 				.orElseThrow(()->
 				new ResourceNotFoundException
 				("Admin with given id does not exist")),
 				AdminDTO.class) ;	
-		}
+	}
+	
 	@Override
 	public Buyer findBuyerByEmail(String email) {
-
-		
 		Buyer buyer=buyerdao.findByEmail(email);
 		buyer.getBuyerReview().size();
 		buyer.getBuyerPayment().size();
@@ -50,14 +48,21 @@ public class AdminServiceImpl implements AdminService{
 		buyer.getBuyerIssues().size();
 		return buyer;
 	}
+	
 	@Override
 	public Freelancer findFreelancerByEmail(String email) {
 		Freelancer freelancer=freelancerdao.findByEmail(email);
-	//	freelancer.getGigs().size();
-//		freelancer.getFreelancerReview().size();
-//		freelancer.getFreelancerPayment().size();
-//		freelancer.getFreelanceIssues().size();
-//		freelancer.getFreelancerOrders().size();
+		freelancer.getGigs().size();
+		freelancer.getFreelancerReview().size();
+		freelancer.getFreelancerPayment().size();
+		freelancer.getFreelanceIssues().size();
+		freelancer.getFreelancerOrders().size();
 		return freelancer;
+	}
+	
+	@Override
+	public String deleteFreelancer(Long freelancerId) {
+		freelancerdao.deleteById(freelancerId);
+		return null;
 	}
 }
